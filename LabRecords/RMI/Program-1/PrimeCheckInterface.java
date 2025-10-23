6844c68f3f393d0e998eedfaf06a60e2d316511e
Maeve Fernandes <maeve.fernandes.7@gmail.com>
2025-10-24 00:48:52 +0530

    Prime Number RMI

--- LabRecords/RMI/Program-1/PrimeCheckInterface.java
+++ LabRecords/RMI/Program-1/PrimeCheckInterface.java
@@ -1,10 +1,6 @@
-package LabRecords.RMI.Program1;
 import java.rmi.RemoteException;
 import java.rmi.Remote;
-
 
 public interface PrimeCheckInterface extends Remote {
     public boolean checkPrime(int number) throws RemoteException;
 }
-
-
